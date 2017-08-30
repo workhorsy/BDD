6e32360c911e8c194e3a9d7c172b8e5203fb57fa
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-08-30 11:47:12 -0700

    Added script to generate new release

---
+++ tools/make_release.sh
@@ -0,0 +1,21 @@
+
+# Stop and exit on error
+set -e
+
+VERSION="1.2.0"
+
+cd ..
+sed 's/$VERSION/'$VERSION'/g' tools/README.md.template > README.md
+
+# Generate documentation
+dmd -c -D source/BDD.d -Df=docs/$VERSION/index.html
+exit
+git add docs/$VERSION/
+
+# Create release
+git commit -a -m "Release $VERSION"
+git push
+
+# Create and push tag
+git tag v$VERSION -m "Release $VERSION"
+git push --tags
